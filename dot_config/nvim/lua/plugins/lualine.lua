c734ce63957321bf4adf468559b88b8d562dc67a
Pablo Kebees <pablo@i-aspect.com>
2025-02-06 11:39:26 +0100

    Add .config/nvim/lua/plugins/lualine.lua

---
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        -- Overwrite time section, to hide time
+        lualine_z = {},
+      },
+    },
+  },
+}
