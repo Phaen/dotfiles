dd8cf5dcd7cbf75ca33f4343be99246d6ec04e8d
Pablo Kebees <pablo@i-aspect.com>
2025-03-13 16:34:54 +0100

    Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/editor.goto-preview.lua Update .config/nvim/lazy-lock.json

--- dot_config/exact_nvim/exact_lua/exact_plugins/editor.goto-preview.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/editor.goto-preview.lua
@@ -1,21 +1,21 @@
 return {
-  "rmagatti/goto-preview",
-  event = "BufEnter",
-  opts = {
-    references = {
-      provider = "snacks",
+  {
+    "rmagatti/goto-preview",
+    dependencies = { "rmagatti/logger.nvim" },
+    event = "BufEnter",
+    opts = {
+      default_mappings = false,
     },
-    default_mappings = false,
+    keys = function()
+      return {
+        { "gp", "", desc = "Preview", mode = "n" },
+        { "gpd", require("goto-preview").goto_preview_definition, desc = "Preview definition" },
+        { "gpy", require("goto-preview").goto_preview_type_definition, desc = "Preview type definition" },
+        { "gpI", require("goto-preview").goto_preview_implementation, desc = "Preview implementation" },
+        { "gpD", require("goto-preview").goto_preview_declaration, desc = "Preview declaration" },
+        { "gpr", require("goto-preview").goto_preview_references(), desc = "Preview references" },
+        { "gP", require("goto-preview").close_all_win, desc = "Close preview windows" },
+      }
+    end,
   },
-  keys = function()
-    return {
-      { "gp", "", desc = "Preview", mode = "n" },
-      { "gpd", require("goto-preview").goto_preview_definition, desc = "Preview definition" },
-      { "gpy", require("goto-preview").goto_preview_type_definition, desc = "Preview type definition" },
-      { "gpI", require("goto-preview").goto_preview_implementation, desc = "Preview implementation" },
-      { "gpD", require("goto-preview").goto_preview_declaration, desc = "Preview declaration" },
-      { "gpr", require("goto-preview").goto_preview_references(), desc = "Preview references" },
-      { "gP", require("goto-preview").close_all_win, desc = "Close preview windows" },
-    }
-  end,
 }
