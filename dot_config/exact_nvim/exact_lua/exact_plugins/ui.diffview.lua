e00b35f736354c62c3d920b16004dc3ff0c406b1
Pablo Kebees <pablo@i-aspect.com>
2025-03-14 15:30:44 +0100

    Update .config/nvim/lua/plugins/ui.diffview.lua

--- dot_config/exact_nvim/exact_lua/exact_plugins/ui.diffview.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/ui.diffview.lua
@@ -2,28 +2,36 @@ return {
   "sindrets/diffview.nvim",
   opts = function(_, opts)
     local actions = require("diffview.config").actions
-    local conflict_keymaps = {
-      { "n", "<leader>co", false },
-      { "n", "<leader>ct", false },
-      { "n", "<leader>cb", false },
-      { "n", "<leader>ca", false },
-      { "n", "<leader>cn", false },
-      { "n", "<leader>cO", false },
-      { "n", "<leader>cT", false },
-      { "n", "<leader>cB", false },
-      { "n", "<leader>cA", false },
-      { "n", "<leader>cN", false },
-      { "n", "<leader>gco", actions.conflict_choose("ours"), { desc = "Choose 'ours' version for conflict" } },
-      { "n", "<leader>gct", actions.conflict_choose("theirs"), { desc = "Choose 'theirs' version for conflict" } },
-      { "n", "<leader>gcb", actions.conflict_choose("base"), { desc = "Choose 'base' version for conflict" } },
-      { "n", "<leader>gca", actions.conflict_choose("all"), { desc = "Choose 'all' versions for conflict" } },
-      { "n", "<leader>gcn", actions.conflict_choose("none"), { desc = "Choose 'none' version for conflict" } },
-      { "n", "<leader>gcO", actions.conflict_choose_all("ours"), { desc = "Choose 'ours' for all conflicts" } },
-      { "n", "<leader>gcT", actions.conflict_choose_all("theirs"), { desc = "Choose 'theirs' for all conflicts" } },
-      { "n", "<leader>gcB", actions.conflict_choose_all("base"), { desc = "Choose 'base' for all conflicts" } },
-      { "n", "<leader>gcA", actions.conflict_choose_all("all"), { desc = "Choose 'all' for all conflicts" } },
-      { "n", "<leader>gcN", actions.conflict_choose_all("none"), { desc = "Choose 'none' for all conflicts" } },
-    }
+    local options = { o = "ours", t = "theirs", b = "base", a = "all", n = "none" }
+
+    local conflict_keymaps = {}
+    for key, option in pairs(options) do
+      -- Disable <leader>c mappings
+      table.insert(conflict_keymaps, { "n", "<leader>c" .. key, false })
+      table.insert(conflict_keymaps, { "n", "<leader>c" .. string.upper(key), false })
+
+      -- Add <leader>gc mappings for single
+      table.insert(
+        conflict_keymaps,
+        {
+          "n",
+          "<leader>gc" .. key,
+          actions.conflict_choose(option),
+          { desc = string.format("Choose '%s' version for conflict", option) },
+        }
+      )
+
+      -- Add <leader>gc mappings for all
+      table.insert(
+        conflict_keymaps,
+        {
+          "n",
+          "<leader>gc" .. string.upper(key),
+          actions.conflict_choose_all(option),
+          { desc = string.format("Choose '%s' for all conflicts", option) },
+        }
+      )
+    end
 
     return vim.tbl_deep_extend("force", opts, {
       keymaps = {
