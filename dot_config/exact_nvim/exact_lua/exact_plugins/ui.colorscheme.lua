366ba494d837b6512c691dce61522f0d03d9e2c3
Pablo Kebees <pablo@i-aspect.com>
2025-09-15 14:57:46 +0200

    Update .config/nvim/lua/plugins/ui.colorscheme.lua Update .config/nvim/lazy-lock.json

--- dot_config/exact_nvim/exact_lua/exact_plugins/ui.colorscheme.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/ui.colorscheme.lua
@@ -1,11 +1,25 @@
 ---@type LazyPluginSpec
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
+  -- catppuccin theme
+  {
+    "catppuccin/nvim",
+    version = "v1.11.0", -- the master branch changed 'get()' function,
+    name = "catppuccin",
+    -- lazy = true,
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = vim.g.transparent_background,
+      float = {
+        transparent = vim.g.transparent_background, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "catppuccin",
     },
   },
 }
