704f98a53607f74ff208036e6ce0fb6b7e404e5e
Pablo Kebees <pablo@i-aspect.com>
2025-11-11 10:31:50 +0100

    Update .config/nvim/lua/plugins/lang.blade.lua Update .config/nvim/lua/plugins/ui.snacks.picker.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Add .config/nvim/.claude/.keep

--- dot_config/exact_nvim/exact_lua/exact_plugins/ui.snacks.picker.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/ui.snacks.picker.lua
@@ -1,5 +1,14 @@
 return {
   "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          include = { ".env*" },
+        },
+      },
+    },
+  },
   keys = {
     { "<leader>gc", false },
   },
