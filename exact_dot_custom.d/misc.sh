2acf6bb35c9a4c718f26446f2e0a4331be027b75
Pablo Kebees <pablo@i-aspect.com>
2026-03-03 17:17:29 +0100

    Update .custom.d/misc.sh

--- exact_dot_custom.d/misc.sh
+++ exact_dot_custom.d/misc.sh
@@ -5,8 +5,17 @@ function cdf() {
   cd "$(find . -type d -mindepth 1 -maxdepth 1 | xargs -I{} basename {} | fzf --query="${1:-}" --preview="ls -A {}" --select-1 --height 20)"
 }
 
+# Easy auto-restart with 1s sleep
+persistent() {
+  while true; do
+    "$@"
+    echo "--- Exited ($?), reconnecting in 1s ---"
+    sleep 1
+  done
+}
+
+# Easy access to Python venv
 alias venv="source venv/bin/activate"
 
-persistent() {
-    while true; do "$@"; echo "--- Exited ($?), reconnecting in 1s ---"; sleep 1; done
-}
+# Bypass xterm-kitty during sudo, when unavailable on sudo'd user
+alias sudou='sudo TERM=xterm-256color -iu'
